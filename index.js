0121c92a1d6f25963adb0ad4eea3659fbd981d1b
JonathanWaller <waller.j27@gmail.com>
2020-02-20 21:05:03 -0600

    initialized toy problems sheet

---
+++ index.js
@@ -0,0 +1,10 @@
+1. Given a number, return 'Even' if it is an even number...'Odd' if it is odd
+
+
+function even_or_odd(number) {
+  if (number%2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
