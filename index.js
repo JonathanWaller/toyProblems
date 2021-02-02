3d0b3da874299b661f4fb2859675507dee548128
JonathanWaller <waller.j27@gmail.com>
2021-02-01 20:21:37 -0600

    finished top 10

--- index.js
+++ index.js
@@ -99,3 +99,76 @@ class SmallestIntegerFinder {
 }
 
 8. Remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  return x.split('').filter(e=>e.trim()).join('')
+}
+
+OR
+
+function noSpace(x){
+  return x.split(' ').join('') 
+}
+
+OR
+
+str = str.replace(/\s/g, '');
+
+9. Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+var summation = function (num) {
+  let sum = 0;
+  for( let i=1; i<=num; i++ ) {
+    sum += i
+  }
+  return sum;
+}
+
+OR
+
+function summation(num) {
+  return num * (num + 1) / 2
+}
+
+10. Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+function squareSum(numbers){
+  let sum = 0;
+  for( let x of numbers ) {
+    sum += x*x;
+  }
+  return sum;
+}
+
+OR 
+
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
+
+11. Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+
+function countSheeps(arrayOfSheep) {
+  let sum = 0;
+  for( let x of arrayOfSheep ) {
+    if( x !== false && x !== null && x !== undefined ) {
+      sum++;
+    }
+  }
+  return sum;
+}
+
+OR
+
+function countSheeps(arrayOfSheep) {
+  let sum = 0;
+  for( i=0; i<arrayOfSheep.length; i++ ) {
+    if( arrayOfSheep[i] === true ) {
+      sum ++;
+    }
+  }
+  return sum;
+}
